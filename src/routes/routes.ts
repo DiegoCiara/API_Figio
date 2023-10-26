d74bcfdbe0cedb5048ba9f868143db0546b40cda
Diego Antonio Ciara <114518504+DiegoCiara@users.noreply.github.com>
2023-10-26 10:12:15 -0300

    Initial commit

---
+++ src/routes/routes.ts
@@ -0,0 +1,30 @@
+import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
+import DealRoutes from '@routes/deal.routes';
+import Router from 'express';
+import AuthRoutes from './auth.routes';
+import AutomationRoutes from './automation.routes';
+import MailerRoutes from './mailer.routes';
+import CompanyRoutes from './company.routes';
+import ContactRoutes from './contact.routes';
+import PipelineRoutes from './pipeline.routes';
+import FunnelRoutes from './funnel.routes';
+import UserRoutes from './user.routes';
+
+const routes = Router();
+
+routes.get('/', (req, res) => {
+  res.json({ API: 'Terceiro Semetre' });
+});
+
+// prefix
+routes.use('/auth', AuthRoutes);
+routes.use('/user', UserRoutes); // middlewares estão no UserRoutes;
+routes.use('/pipeline', PipelineRoutes);
+routes.use('/automation', AutomationRoutes);
+routes.use('/mail', MailerRoutes);
+routes.use('/funnel', FunnelRoutes);
+routes.use('/company', CompanyRoutes);
+routes.use('/contact', ContactRoutes);
+routes.use('/deal', DealRoutes);
+
+export default routes;
